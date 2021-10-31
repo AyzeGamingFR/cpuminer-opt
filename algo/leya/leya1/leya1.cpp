1557f0438283295628668bfeaa937874c09fc9a8
AyzeLYC <35877385+AyzeGamingFR@users.noreply.github.com>
2021-10-31 17:05:27 +0100

    Created leya1.cpp

---
+++ algo/leya/leya1/leya1.cpp
@@ -0,0 +1,17 @@
+#include <string>
+
+namespace leya1 {
+    
+    int hash(string datas) {
+        
+        
+        
+    };
+    
+    int brute_force(string datas) {
+        
+        
+        
+    };
+    
+}
