37de274eed89d1df7d707afaf1521e40d7cd7e95
AyzeLYC <35877385+AyzeGamingFR@users.noreply.github.com>
2021-11-01 11:36:09 +0100

    Updated leya1.cpp

--- algo/leya/leya1/leya1.cpp
+++ algo/leya/leya1/leya1.cpp
@@ -2,13 +2,43 @@
 
 namespace leya1 {
     
-    int hash(string datas) {
+    static const consts = ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""];
+    int i = 1
+    
+    double p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16;
+    int ip1, ip2, ip3, ip4, ip5, ip6, ip7, ip8, ip9, ip10, ip11, ip12, ip13, ip14, ip15, ip16;
+    int hp1, hp2, hp3, hp4, hp5, hp6, hp7, hp8, hp9, hp10, hp11, hp12, hp13, hp14, hp15, hp16;
+    
+    int hash(string datas) { // 1024 bits hashing algorithm
         
-        
+        for (datas.length() > i * 1024) {
+            
+            i += 1
+            
+        };
+        for (datas.length() != i * 1024) {
+            
+            datas[datas.length()] = "0"
+            
+        };
+        ip1 = datas[0 : (i * 64 - 1)];
+        ip2 = datas[(i * 64) : (i * 64 + 1)];
+        ip3 = "";
+        ip4 = "";
+        ip5 = "";
+        ip6 = "";
+        ip7 = "";
+        ip8 = "";
+        ip9 = "";
+        ip10 = "";
+        ip11 = "";
+        ip12 = "";
+        ip13 = "";
+        ip14 = "";
+        ip16 = "";
         
     };
-    
-    int brute_force(string datas) {
+    int brute_force(string hash) {
         
         
         
